555729b8a856b6a19d6873b59a62a0f7ef31b2f3
akproggen <akmurman@gmail.com>
2025-05-26 13:53:00 +0200

    container addded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from "./layout/header/header.component";
 import { FooterComponent } from "./layout/footer/footer.component";
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, HeaderComponent, FooterComponent, BrowserModule, RouterModule],
+  imports: [RouterModule,CommonModule, HeaderComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
