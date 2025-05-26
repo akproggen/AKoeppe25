7afed42482efbc37436f698a957a76c1ad01f8d9
akproggen <akmurman@gmail.com>
2025-05-26 13:04:04 +0200

    header und footer und routs added

---
+++ src/app/pages/newslist/newslist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newslist',
+  imports: [],
+  templateUrl: './newslist.component.html',
+  styleUrl: './newslist.component.scss'
+})
+export class NewslistComponent {
+
+}
